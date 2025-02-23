da2c883a5aa2f74c9213cb101722fa956621bf62
olcay <olcayeryigit@hotmail.com>
2025-02-24 01:15:56 +0300

    -run-buildde-hata-vermeyen-ssr

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,30 +1,27 @@
-import { Metadata, ResolvingMetadata } from 'next'; // Metadata ve ResolvingMetadata tipleri
+'use client'
+import * as React from 'react'
 
-type BlogPageProps = {
-  slug: string;
-};
-
-// Metadata oluşturuluyor
-export async function generateMetadata(
-  { params }: { params: Promise<{ slug: string }> }, // params Promise olarak alınır
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const resolvedParams = await params; // params'ı çözümle
-  const { slug } = resolvedParams;
-
-  // Başlık oluşturuluyor
-  return {
-    title: `Blog Post: ${slug}`, // Slug'a göre başlık
-  };
+type Props = {
+  params: Promise<{ slug: string }>
 }
 
-const BlogPage: React.FC<BlogPageProps> = ({ slug }) => {
-  return (
-    <div>
-      <h1>Blog Post: {slug}</h1>
-      <p>Here is the content for the blog post with slug: {slug}</p>
-    </div>
-  );
-};
+const Page: React.FC<Props> = ({ params }) => {
+  const [slug, setSlug] = React.useState<string | null>(null);
 
-export default BlogPage;
+  React.useEffect(() => {
+    const fetchParams = async () => {
+      const resolvedParams = await params; // params'ı çözüme kavuşturuyoruz
+      setSlug(resolvedParams.slug); // id'yi state'e set ediyoruz
+    };
+
+    fetchParams();
+  }, [params]);
+
+  if (slug=== null) {
+    return <p>Yükleniyor...</p>;
+  }
+
+  return <p>Slug: {slug}</p>;
+}
+
+export default Page;
