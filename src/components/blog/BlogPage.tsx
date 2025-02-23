60f906049b059ba9c7300fad92aca9b3cf5f3c27
olcay <olcayeryigit@hotmail.com>
2025-02-24 00:53:48 +0300

    v-ssr-deneme

--- src/components/blog/BlogPage.tsx
+++ src/components/blog/BlogPage.tsx
@@ -1,15 +1,20 @@
-import { Metadata } from 'next'; // Metadata tipi
+import { Metadata, ResolvingMetadata } from 'next'; // Metadata ve ResolvingMetadata tipleri
 
 type BlogPageProps = {
   slug: string;
 };
 
 // Metadata oluşturuluyor
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-  const { slug } = params;
+export async function generateMetadata(
+  { params }: { params: Promise<{ slug: string }> }, // params Promise olarak alınır
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const resolvedParams = await params; // params'ı çözümle
+  const { slug } = resolvedParams;
 
+  // Başlık oluşturuluyor
   return {
-    title: `Blog Post: ${slug}`, // Slug'a göre başlık oluşturuluyor
+    title: `Blog Post: ${slug}`, // Slug'a göre başlık
   };
 }
 
