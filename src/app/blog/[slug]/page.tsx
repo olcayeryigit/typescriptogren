9dc40ea6b8a3d678c981408db368255ebbeccc08
olcay <olcayeryigit@hotmail.com>
2025-02-23 21:51:09 +0300

    v-dynamic-metadata-test

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import BlogPage from '@/components/blog/BlogPage';
-import { Metadata } from 'next';
 
 type PageProps = {
   params: Promise<{ slug: string }>;  // Slug parametresi Promise olarak alınıyor
 };
 
-export const metadata: Metadata = {
-  title: 'Blog | My Website',  // Sabit başlık
-  description: 'Web geliştirme, yazılım, eğitim ve daha fazlası hakkında derinlemesine makaleler ve rehberler. En güncel içerikleri keşfedin ve gelişen teknoloji dünyasında kendinizi geliştirin.',
-};
+
 
 const Page = ({ params }: PageProps) => {
   return (
