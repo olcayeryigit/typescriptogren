0a85967238f536d0f4f8644c58baac1f47d82e73
olcay <olcayeryigit@hotmail.com>
2025-04-30 13:53:17 +0300

    v

--- src/components/common/MainParagraph.tsx
+++ src/components/common/MainParagraph.tsx
@@ -7,9 +7,8 @@ interface MainParagraphProps {
 const MainParagraph: React.FC<MainParagraphProps> = ({ text }) => {
   return (
     <p className="text-sm  md:text-lg text-custom-gray text-center px-6 sm:px-8 py-3 sm:py-4 leading-relaxed bg-custom-blue-3 my-4 sm:my-6">
-    {text}
-  </p>
-  
+      {text}
+    </p>
   );
 };
 
